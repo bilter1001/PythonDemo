667b13a50eb6dfc83396dbdb9167432a99dd12c4
shenzhixiong_313322 <bilter1001@163.com>
2022-02-18 15:37:15 +0800

    【A】添加示例代码

---
+++ Demo1.py
@@ -0,0 +1,62 @@
+import sys
+
+
+
+
+
+
+#字符串
+str='123456789'
+ 
+print(str)                 # 输出字符串
+print(str[0:-1])           # 输出第一个到倒数第二个的所有字符
+print(str[0])              # 输出字符串第一个字符
+print(str[2:5])            # 输出从第三个开始到第五个的字符
+print(str[2:])             # 输出从第三个开始后的所有字符
+print(str[1:5:2])          # 输出从第二个开始到第五个且每隔一个的字符（步长为2）
+print(str * 2)             # 输出字符串两次
+print(str + '你好')         # 连接字符串
+ 
+print('------------------------------')
+ 
+print('hello\nrunoob')      # 使用反斜杠(\)+n转义特殊字符
+print(r'hello\nrunoob')     # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+
+print(sys.path.pop(1))
+
+#list示例代码
+lstWord=['开心',222,'谢谢','没有','东西','123']
+print(lstWord)
+
+print(lstWord[:-1])
+print(lstWord[::-1])#反转list
+
+#list示例代码 反转list
+def reverseWords(input):
+     
+    # 通过空格将字符串分隔符，把各个单词分隔为列表
+    inputWords = input.split(" ")
+ 
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],  
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    inputWords=inputWords[-1::-1]
+ 
+    # 重新组合字符串
+    output = ' '.join(inputWords)
+     
+    return output
+ 
+if __name__ == "__main__":
+    input = 'I like runoob'
+    rw = reverseWords(input)
+    print(rw)
+
+
+
+
+
