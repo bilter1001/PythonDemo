f7574d3dfa53c1651c8622ebb1053a3a88a82b7d
bilter1001 <bilter1001@163.com>
2022-07-17 15:53:14 +0800

    提交网络爬虫的代码

--- 网络爬虫/网站地图爬虫.py
+++ 网络爬虫/网站地图爬虫.py
@@ -1,13 +1,19 @@
 from cgitb import html
 import urllib.request
 from urllib.error import URLError,HTTPError,ContentTooShortError
+import re
 
-def download(url, user_agent='wswp', num_retries=2):
+
+def download(url, user_agent='wswp', num_retries=2, charset='utf-8'):
     print('Downloading:', url)
     request = urllib.request.Request(url)
     request.add_header('User-agent', user_agent)
     try:
-        html = urllib.request.urlopen(request).read()
+        resp = urllib.request.urlopen(request)
+        cs = resp.headers.get_content_charset()
+        if not cs:
+            cs = charset
+        html = resp.read().decode(charset)
     except (URLError,HTTPError,ContentTooShortError) as e:
         print("Download Error:", e.reason)
         html=None
@@ -17,5 +23,19 @@ def download(url, user_agent='wswp', num_retries=2):
                 return download(url, user_agent, num_retries -1)
     return html
 
+
+def crawl_sitemap(url):
+    #download the sitemap file
+    sitemap = download(url)
+    # extract the sitemap links
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    for link in links:
+        html = download(link)
+        # scrape html here
+        # ...
+
 #download('http://httpstat.us/500')
-download('http://meetup.com')
+crawl_sitemap('http://example.python-scraping.com/sitemap.xml')
+
+
+
